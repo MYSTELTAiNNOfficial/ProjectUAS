eeddd0ba0fb78d22edae33caac11039bf6708c47
MYSTELTAiNN <MYSTELTAiNNOfficial@users.noreply.github.com>
2020-12-23 15:48:17 +0700

    Update Main.java
    do (for looping)

--- Main.java
+++ Main.java
@@ -26,16 +26,18 @@ public class Main {
     public static void main(String[] args) {
         Main gate = new Main(); //penghubung method
         Scanner scan = new Scanner(System.in);
-        
+
         int menu;
         String golongan[][][] = {{{}}};
         String barang[] = {"Lampu", "Rice Cooker", "Kulkas", "AC", "Setrika", "Mesin Cuci"}; //[] pertama = tipe,
-        
-        gate.menu(); //panggil menu
-        menu = scan.nextInt();
+        do {
+            gate.menu(); //panggil menu
+            menu = scan.nextInt();
+            
+        } while (true);
     }
-    
-    public void menu (){ //menu awal
+
+    public void menu() { //menu awal
         System.out.println("Penghitung biaya listrik (By Syamsuddin & Averill)");
         System.out.println("");
         System.out.println("===============");
